f85e1c5f695444d854eae89890befc5ad332416e
Raihanudin Rafif <raihanudinrafif@gmail.com>
2024-01-21 15:30:48 +0700

    fix style dashboard

--- src/components/VoteResult.jsx
+++ src/components/VoteResult.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 export default function VoteResult({namaKetua, fotoKetua, namaWakil, fotoWakil, score, onclick}) {
   return (
     <div className='w-[95%] h-full py-3 px-3 flex gap-1 bg-slate-300 rounded-md justify-center items-start relative mx-auto'>
-        <div className='w-[40%] flex flex-col gap-2 justify-center items-center  text-[#202121] rounded-lg' >
-            <h1 className='font-medium'>Ketua</h1>
-            <div className='w-full h-full flex justify-center items-center'>
-                <img src={fotoKetua} className='max-w-[80%] rounded' alt="" />
+        <div className='w-full flex flex-col justify-center items-center'>
+            <div className='w-full flex justify-center items-start mt-2 '>
+                <div className='w-[50%] flex flex-col justify-center items-center gap-1'>
+                    <h1 className='font-medium text-sm text-center'>Ketua</h1>
+                    <p className='max-w-[80%] text-sm font-bold text-ellipsis text-center'>{namaKetua}</p>            
+                </div>
+                <div className=' w-[50%] flex flex-col justify-center items-center gap-1'>
+                    <h1 className='font-medium text-sm text-center'>Wakil Ketua</h1>
+                    <p className='max-w-[80%] text-sm font-bold text-ellipsis text-center'>{namaWakil}</p>            
+                </div>
             </div>
-            <p className='max-w-[80%] text-lg font-semibold text-ellipsis text-center'>{namaKetua}</p>            
-        </div>
-        <div className='w-[40%] flex flex-col gap-2 justify-center items-center  text-[#202121] rounded-lg' >
-            <h1 className='font-medium'>Wakil Ketua</h1>
-            <div className='w-full h-full flex justify-center items-center'>
-                <img src={fotoWakil} className='max-w-[80%] rounded' alt="" />
+            <div className='flex justify-center items-cente'>
+                    <img src={fotoKetua} className='w-[70%] rounded -mx-10' alt="" />
+                    <img src={fotoWakil} className='w-[70%] rounded -mx-10' alt="" />
             </div>
-            <p className='max-w-[90%] text-lg font-semibold text-ellipsis text-center'>{namaWakil}</p>
         </div>
         <div className='w-[20%] h-[30%] flex flex-col gap-2 justify-center items-center bg-[#212121] my-auto  text-[#202121] p-2 rounded-md' >
             <h1 className='text-white'>Total</h1>
